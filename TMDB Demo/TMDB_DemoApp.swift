58eb073203b058bbcecc89c1ed0b35352c4effab
Naga <nag555syam@gmail.com>
2024-05-10 13:01:39 +0530

    Initial Commit

---
+++ TMDB Demo/TMDB_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_DemoApp.swift
+//  TMDB Demo
+//
+//  Created by Naga on 10/05/24.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
